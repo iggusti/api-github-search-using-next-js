9bb4ba17ff19a0b5bb61bb344e3ab622b569ec65
ilhamgg <ilhamgg@telkomuniversity.ac.id>
2023-01-20 07:38:00 +0700

    fixing hasil pencarian

--- components/UserList.tsx
+++ components/UserList.tsx
@@ -6,7 +6,7 @@ export default function UserList({ result }: Props) {
   return (
     <div className="border rounded-[8px] w-full space-y-3 pb-10">
       <p className="text-center p-[20px]">
-        Hasil Pencarian : <span className="font-bold">Budi</span>
+        Hasil Pencarian : <span className="font-bold">{result?.search}</span>
       </p>
       {result?.users.map((user, index) => (
         <UserItem key={index} user={user}></UserItem>
