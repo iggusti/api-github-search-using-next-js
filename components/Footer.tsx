4ff1e93fabd57a2eef73533b29f2bf7ac6991a91
ilhamgg <ilhamgg@telkomuniversity.ac.id>
2023-01-20 07:33:28 +0700

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
   return (
     <div className="text-center p-10 border-t">
-      Belajar Next.js dengan <span className="font-bold">Baris Kode</span>
+      Belajar Next.js dibuat oleh{" "}
+      <span className="font-bold">
+        <Link href={`https://iggusti.github.io/curriculum-vitae/`}>
+          IGGUSTI
+        </Link>
+      </span>
     </div>
   );
 }
